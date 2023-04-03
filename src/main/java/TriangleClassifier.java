71f00c5cc1275823d2be944bd43e6620a4e01bca
viyeumaden <66916286+viyeumaden@users.noreply.github.com>
2023-04-03 16:52:01 +0700

    chua xong

---
+++ src/main/java/TriangleClassifier.java
@@ -0,0 +1,34 @@
+public class TriangleClassifier {
+    private double a;
+    private  double b;
+    private  double c;
+
+    public TriangleClassifier() {
+    }
+
+    public TriangleClassifier(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+     String testTriangle (TriangleClassifier c){
+        double x= c.getA(), y=c.getB(),z=c.getC();
+        if (x==y) {
+            if y=z
+        }
+
+    }
+}
